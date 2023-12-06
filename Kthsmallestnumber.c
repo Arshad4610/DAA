b2009d122e2fe70b3635171de14d08e24ff7622e
Arshad4610 <134123830+Arshad4610@users.noreply.github.com>
2023-12-06 20:41:02 +0530

    Kthsmallestnumber.c

---
+++ Kthsmallestnumber.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+void merge(int a[],int l,int m,int r)
+{
+    int n1=m-l+1;
+    int n2=r-m;
+    int L[n1],R[n2];
+    for(int i=0;i<n1;i++)
+    {
+        L[i]=a[l+i];
+    }
+    for(int j=0;j<n2;j++)
+    {
+        R[j]=a[m+1+j];
+    }
+    int i=0;int j=0;int k=l;
+    while(i<n1 && j<n2)
+    {
+        if(L[i]<R[j])
+        {
+            a[k]=L[i];
+            i++;
+        }
+        else
+        {
+            a[k]=R[j];
+            j++;
+        }
+        k++;
+    }
+    while(i<n1)
+    {
+        a[k]=L[i];
+        i++;
+        k++;
+    }
+    while(j<n2)
+    {
+        a[k]=R[j];
+        j++;
+        k++;
+    }
+}
+void mergesort(int a[],int l,int r)
+{
+    if(l<r)
+    {
+        int m=l+(r-l)/2;
+        mergesort(a,l,m);
+        mergesort(a,m+1,r);
+        merge(a,l,m,r);
+    }
+}
+void print(int a[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+}
+int main()
+{
+    int a[]={12,11,13,5,6,7};
+    int k;
+    int a_size=sizeof(a)/sizeof(a[0]);
+    mergesort(a,0,a_size-1);
+    print(a,a_size);
+    printf("enter kth smallest number");
+    scanf("%d",&k);
+    printf("%d",a[k-1]);
+}
